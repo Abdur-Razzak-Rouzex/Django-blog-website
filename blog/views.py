82a1bb086c06ff7d72a5d41b7ad07305958c59ab
Abdur-Razzak-Rouzex <abdurrazzak1663@gmail.com>
2020-08-18 14:09:50 -0700

    In app blog:1. Created the Post model in models.py 2. Register the post model in admin.py 3. Added the post model in views.py 4. Filtered the timezone in the home template

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'title': 'post 1',
-        'author': 'A Razzak',
-        'content': 'This is the first post of the blog',
-        'date_posted': '20 jan, 2020'
-    },
-    {
-        'title': 'post 2',
-        'author': 'A Rouzex',
-        'content': 'This is the post you want to see, as a second post',
-        'date_posted': '10 jan, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
