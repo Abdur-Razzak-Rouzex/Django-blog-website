0a5a5bf7d136b1d9e4a5221de699481924bdb8c3
Abdur-Razzak-Rouzex <abdurrazzak1663@gmail.com>
2020-08-20 00:40:48 -0700

    log in and log functionalities: login and logout urls are crated in the main project url and added a profile link in the users app urls file, login_redirect url and login_url set up in the setting, log in and logout templates were created and login requirement decorator was added in the users views file to access the profile section of the site

--- web_blog/settings.py
+++ web_blog/settings.py
@@ -1,9 +1,7 @@
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -15,7 +13,6 @@ SECRET_KEY = ')ug=%!0acqd&*f$kv)7nj@k3%4jttk=u)$0(330i1m23qlx3-@'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -61,7 +58,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'web_blog.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -71,7 +67,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -91,7 +86,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -105,10 +99,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login'
